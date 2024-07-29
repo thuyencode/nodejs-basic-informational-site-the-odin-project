0707d8a9e523e4214df34d435f3ff6ef01e644f0
Thuyen Code <thuyencode@proton.me>
2024-07-29 15:59:57 +0700

    Change default configs and helper functions for routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,16 @@
 import { HTTPMethod } from 'http-method-enum'
+import HTTP_STATUS_CODES from 'http-status-enum'
 import path from 'path'
 import { DIR_NAME } from './constants.js'
 
 const defaultConfig = {
   method: HTTPMethod.GET,
-  contentType: 'Content-Type: text/html; charset=utf-8',
-  filePath: path.join(DIR_NAME, 'public', 'index.html')
+  statusCode: HTTP_STATUS_CODES.default.OK,
+  contentType: { 'Content-Type': 'text/html; charset=utf-8' },
+  filePath: path.join(path.dirname(DIR_NAME), 'public', 'index.html')
 }
 
-export const routes = Object.freeze({
+export const routes = {
   '/': defaultConfig,
   '/about': {
     ...defaultConfig,
@@ -22,10 +24,18 @@ export const routes = Object.freeze({
     ...defaultConfig,
     filePath: path.join(DIR_NAME, 'public', '404.html')
   }
-})
+} as const
 
 export type Path = keyof typeof routes
 
 export function getPaths(): Path[] {
   return Object.keys(routes) as Path[]
+}
+
+export type Route = typeof defaultConfig
+
+export function getPath(path: string): Route | undefined {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return routes[path]
 }
