dcab752f542872241ba07b392baaf08fcfeb903d
Thuyen Code <thuyencode@proton.me>
2024-07-29 16:04:15 +0700

    Handle routing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,54 @@
-import http from 'http'
+import { existsSync } from 'fs'
+import fs from 'fs/promises'
+import http, { IncomingMessage, ServerResponse } from 'http'
+import { HttpError, InternalServerError, NotFound } from './errors/index.js'
+import { getPath } from './routes.js'
+
+/**
+ * Logger middleware
+ *
+ * @param {IncomingMessage} req
+ * @param {ServerResponse} res
+ * @param {CallableFunction} next
+ */
+function logger(
+  req: IncomingMessage,
+  res: ServerResponse,
+  next: CallableFunction
+) {
+  console.info(`${req.method} ${req.url}`)
+  next()
+}
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify({ message: 'Hello World!' }))
+  logger(req, res, async () => {
+    try {
+      const route = getPath(req.url as string)
+
+      if (route === undefined) {
+        throw new NotFound()
+      }
+
+      if (!existsSync(route.filePath)) {
+        throw new InternalServerError()
+      }
+
+      const data = await fs.readFile(route.filePath)
+
+      res.writeHead(route.statusCode, route.contentType)
+      res.write(data)
+      res.end()
+    } catch (error) {
+      if (error instanceof HttpError) {
+        res.writeHead(error.statusCode, { 'Content-Type': 'application/json' })
+        res.end(
+          JSON.stringify({
+            error: { statusCode: error.statusCode, message: error.message }
+          })
+        )
+      }
+    }
+  })
 })
 
 const PORT = process.env.PORT
